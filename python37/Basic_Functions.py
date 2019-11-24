553764211ce708fe8d373ad5299593b06a282c53
jonlamca <54996874+jonlamca@users.noreply.github.com>
2019-11-24 16:06:39 -0500

    Update user text collection
    Fix json double encoding and include additional features

--- python37/Basic_Functions.py
+++ python37/Basic_Functions.py
@@ -5,8 +5,9 @@ def writeJsonFile(data: object = None, name: object = "UnKnown", folder: object 
     if data is not None:
         print("Writing JSON file ‘{}/{}.json‘......".format(folder, name))
         json_data = json.dumps(data)
+        print(json_data)
         with open('{}/{}.json'.format(folder, name), 'w') as f:
-            json.dump(json_data, f)
+            f.write(json_data)
         print("JSON file ‘{}/{}.json‘ has been written successfully.".format(folder, name))
     else:
         print("Please provide data.")
@@ -16,8 +17,7 @@ def readJsonFile(name="UnKnown", folder="data"):
     with open('{}/{}.json'.format(folder, name)) as json_file:
         dataset = json.load(json_file)
         return dataset
-
-
+        
 def printDictionary(dictionary):
     for key in dictionary.keys():
         print(format(key) + ": " + format(dict[key]))
